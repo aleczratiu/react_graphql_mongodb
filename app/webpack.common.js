aa4a9280fd402e86585a138de254c7948cbd1d33
Alex Ratiu <rr.alecz@yahoo.com>
2018-11-10 20:37:05 +0200

    Webpack setup

---
+++ app/webpack.common.js
@@ -0,0 +1,119 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const srcPath = path.resolve(__dirname, 'src');
+const distPath = path.resolve(__dirname, 'dist');
+const publicPath = path.resolve(__dirname, 'public');
+
+const vendors = ['react', 'react-dom', 'react-apollo', 'react-redux', 'react-router', 'babel-polyfill'];
+
+module.exports = {
+    entry: {
+        bundle: [
+            'babel-polyfill',
+            path.resolve(srcPath, 'index.js'),
+        ],
+        vendors,
+    },
+    watchOptions: {
+        ignored: /node_modules/,
+    },
+    output: {
+        path: distPath,
+        filename: '[name].[hash].bundle.js',
+        chunkFilename: '[name].[chunkhash].bundle.js',
+        publicPath: '/',
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                    enforce: true,
+                },
+            },
+        },
+        runtimeChunk: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: srcPath,
+                loader: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                include: srcPath,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.svg$/,
+                include: srcPath,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                    {
+                        loader: 'svg-react-loader',
+                        options: {
+                            svgo: {
+                                plugins: [
+                                    { cleanupEnableBackground: true },
+                                    { collapseGroups: true },
+                                    { convertTransform: true },
+                                    { removeComments: true },
+                                    { removeEmptyAttrs: true },
+                                    { removeEmptyContainers: true },
+                                    { removeEmptyText: true },
+                                    { removeHiddenElems: true },
+                                    { removeMetadata: true },
+                                    { removeUnknownsAndDefaults: true },
+                                    { removeUselessDefs: true },
+                                    { removeUselessStrokeAndFill: true },
+                                ],
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            Actions: path.resolve(srcPath, 'actions'),
+            Apollo: path.resolve(__dirname, 'apollo'),
+            Components: path.resolve(srcPath, 'components'),
+            Config: path.resolve(srcPath, 'config'),
+            Constants: path.resolve(srcPath, 'constants'),
+            Containers: path.resolve(srcPath, 'containers'),
+            Middleware: path.resolve(srcPath, 'middleware'),
+            Models: path.resolve(srcPath, 'models'),
+            Reducers: path.resolve(srcPath, 'reducers'),
+            Selectors: path.resolve(srcPath, 'selectors'),
+            Tests: path.resolve(srcPath, 'tests'),
+            Utils: path.resolve(srcPath, 'utils'),
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(publicPath, 'index.html'),
+            filename: 'index.html',
+            favicon: path.resolve(publicPath, 'favicon.ico'),
+            title: 'SVGator',
+            inject: true,
+            minify: {
+                collapseInlineTagWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+            },
+        }),
+    ],
+    // node: {
+    //     fs: 'empty',
+    // },
+};
---
+++ app/webpack.dev.js
@@ -0,0 +1,56 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const srcPath = path.resolve(__dirname, 'src');
+const distPath = path.resolve(__dirname, 'dist');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devServer: {
+        clientLogLevel: 'none',
+        contentBase: distPath,
+        historyApiFallback: {
+            disableDotRule: true,
+        },
+        hot: true,
+        host: 'app.svgator',
+        port: 80,
+        public: 'app.svgator',
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(css|sass|scss)$/,
+                include: srcPath,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: true,
+                            localIdentName: '[name]_[local]',
+                        },
+                    },
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV) || JSON.stringify('development'),
+            },
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+});
