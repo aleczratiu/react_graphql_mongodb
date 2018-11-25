a0dab685aad85bbb6ab7f9a6383b617dbcb7b98f
Tarr.Lorant <tarr.lorant@smartketer.net>
2018-11-25 04:36:13 +0200

    error style

--- app/src/components/core/Error/Error.js
+++ app/src/components/core/Error/Error.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Error = ({ error }) => <p style={{ 'font-size': '16px', 'margin': '39px', 'color': 'red' }}>Error: {JSON.stringify(error, null, 2)}</p>;
+const Error = ({ error }) => <p style={{ 'font-size': '14px', 'margin': '10px auto', 'color': 'red' }}>Error: {JSON.stringify(error, null, 2)}</p>;
 
 export default Error;
